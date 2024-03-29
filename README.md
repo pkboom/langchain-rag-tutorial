3a58946eb41e36fe1ff06aa893943c281396d148
Keunbae Park <pkboom@users.noreply.github.com>
2024-03-29 16:50:26 -0400

    wip

--- README.md
+++ README.md
@@ -31,3 +31,9 @@ python query_data.py "How does Alice meet the Mad Hatter?"
 ```
 
 You'll also need to set up an OpenAI account (and set the OpenAI key in your environment variable) for this to work.
+
+# Weaviate
+
+Docker
+
+https://weaviate.io/developers/weaviate/installation/docker-compose
--- playground2.py
+++ playground2.py
@@ -1,54 +1,12 @@
-import dspy
-from dotenv import load_dotenv, find_dotenv
-import os
-from chromadb.utils.embedding_functions import OpenAIEmbeddingFunction
-from dspy.retrieve.chromadb_rm import ChromadbRM
+import re
 
-_ = load_dotenv(find_dotenv())  # read local .env file
+# Split document into single sentences
+chunks = []
+with open("./data/paul_graham_essay.txt", "r", encoding="utf-8") as file:
+    text = file.read()
+    # split by sentence
+    sentences = re.split(r"[\r\n]+", text)
+    sentences = [sentence.strip() for sentence in sentences if sentence.strip()]
+    chunks.extend(sentences)
 
-llm = dspy.OpenAI(
-    model="gpt-3.5-turbo", api_key=os.environ["OPENAI_API_KEY"], max_tokens=300
-)
-
-embedding_function = OpenAIEmbeddingFunction(
-    api_key=os.environ.get("OPENAI_API_KEY"), model_name="text-embedding-ada-002"
-)
-
-retriever_model = ChromadbRM(
-    "alice_in_wonderland",
-    "chroma",
-    embedding_function=embedding_function,
-    k=5,
-)
-
-# results = retriever_model("How does Alice meet the Mad Hatter?", k=5)
-# print(results)
-# quit()
-
-# for result in results:
-#     print("Document:", result.long_text, "\n")
-
-
-class RAG(dspy.Module):
-    def __init__(self, num_passages=3):
-        super().__init__()
-
-        dspy.settings.configure(lm=llm, rm=retriever_model)
-        self.retrieve = dspy.Retrieve(k=num_passages)
-        self.generate_answer = dspy.ChainOfThought("context, question -> answer")
-
-    def forward(self, question):
-        context = self.retrieve(question).passages
-        answer = self.generate_answer(context=context, question=question)
-        print("Context:", context)
-        print("Answer:", answer)
-        print("Question:", question)
-
-        return answer
-
-
-rag = RAG()  # zero-shot, uncompiled version of RAG
-
-answer = rag("How does Alice meet the Mad Hatter?").answer
-
-print(answer)
+print(chunks)
